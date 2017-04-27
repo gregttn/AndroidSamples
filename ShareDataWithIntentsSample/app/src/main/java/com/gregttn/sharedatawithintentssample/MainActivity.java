141d89618e656f70984d47d6e4501874f40a6a3b
greg <gregttn@gregs-MacBook-Pro.local>
2017-04-27 08:48:59 +0100

    Simple intent to trigger message sharing.

--- ShareDataWithIntentsSample/app/src/main/java/com/gregttn/sharedatawithintentssample/MainActivity.java
+++ ShareDataWithIntentsSample/app/src/main/java/com/gregttn/sharedatawithintentssample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.gregttn.sharedatawithintentssample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onShareButtonClicked(View view) {
+        TextView textToShareTextView = (TextView) findViewById(R.id.textToShare);
+        String textToShare = textToShareTextView.getText().toString();
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/*");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, textToShare);
+
+        startActivity(shareIntent);
     }
 }
