1f662c63ac4d43be83afd40d1115ae6aff8f97bc
gregttn <gregttn@gmail.com>
2017-05-10 21:24:02 +0100

    Basic UI for requesting and displaying photo.

--- TakePhotosSample/app/src/main/java/com/gregttn/takephotossample/MainActivity.java
+++ TakePhotosSample/app/src/main/java/com/gregttn/takephotossample/MainActivity.java
@@ -1,18 +1,39 @@
 package com.gregttn.takephotossample;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final Integer REQUEST_PHOTO_FLAG = 345;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boolean hasCamera = getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+
+        if (!hasCamera) {
+            Button takePhotoButton = (Button) findViewById(R.id.take_photo_button);
+            takePhotoButton.setEnabled(false);
+
+            Toast.makeText(this, "CAMERA is not available on this device!", Toast.LENGTH_LONG).show();
+        }
     }
 
-    public void onTakePictureCliced(View view) {
+    public void onTakePictureClicked(View view) {
+        Intent requestPhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
+        boolean canTakePicture = requestPhoto.resolveActivity(getPackageManager()) != null;
+
+        if (canTakePicture) {
+            startActivityForResult(requestPhoto, REQUEST_PHOTO_FLAG);
+        }
     }
 }
