da191616a677447b739672359b8a2cacce26fac3
greg <gregttn@gmail.com>
2017-06-13 08:56:14 +0100

    added two pane layout for large screens

--- MultiScreenSizeSupportSample/app/src/main/java/com/gregttn/multiscreensizesupportsample/MainActivity.java
+++ MultiScreenSizeSupportSample/app/src/main/java/com/gregttn/multiscreensizesupportsample/MainActivity.java
@@ -1,15 +1,18 @@
 package com.gregttn.multiscreensizesupportsample;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.gregttn.multiscreensizesupportsample.model.Article;
 import com.gregttn.multiscreensizesupportsample.model.ArticleRepository;
 
 public class MainActivity extends AppCompatActivity implements ArticleListFragment.OnArticleSelectedListener {
 
     private ArticleRepository articleRepository;
+    private ArticleViewFragment articleViewFragment;
+
+    private boolean hasTwoPanes = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,12 @@ public class MainActivity extends AppCompatActivity implements ArticleListFragme
         ArticleListFragment articleListFragment = (ArticleListFragment) getSupportFragmentManager().findFragmentById(R.id.article_list_fragment);
         articleRepository = new ArticleRepository();
         articleListFragment.updateArticles(articleRepository.getArticles());
+
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.article_view_fragment);
+        if(fragment != null) {
+            hasTwoPanes = true;
+            articleViewFragment = (ArticleViewFragment) fragment;
+        }
     }
 
     @Override
@@ -27,9 +36,13 @@ public class MainActivity extends AppCompatActivity implements ArticleListFragme
     }
 
     private void showArticleView(int articleId) {
-        Intent intent = new Intent(this, ArticleViewActivity.class);
-        intent.putExtra("articleId", articleId);
+        if(hasTwoPanes) {
+            articleViewFragment.displayArticle(articleRepository.getArticleAt(articleId));
+        } else {
+            Intent intent = new Intent(this, ArticleViewActivity.class);
+            intent.putExtra("articleId", articleId);
 
-        startActivity(intent);
+            startActivity(intent);
+        }
     }
 }
