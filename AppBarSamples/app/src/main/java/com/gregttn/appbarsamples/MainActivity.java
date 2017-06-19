7800ba927ee072c4de20921cc2b01f215beb3ce5
gregttn <gregttn@gmail.com>
2017-06-19 20:42:12 +0100

    Added menu with two icons to the toolbar.

--- AppBarSamples/app/src/main/java/com/gregttn/appbarsamples/MainActivity.java
+++ AppBarSamples/app/src/main/java/com/gregttn/appbarsamples/MainActivity.java
@@ -3,6 +3,10 @@ package com.gregttn.appbarsamples;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.bookmark:
+                Toast.makeText(this, "Selected bookmark", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.settings:
+                Toast.makeText(this, "Selected settings", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
