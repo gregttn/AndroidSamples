339db97e7773b725929a4349b615821b62fcadec
greg <gregttn@gmail.com>
2017-06-20 08:59:47 +0100

    added search view to the toolbar

--- AppBarSamples/app/src/main/java/com/gregttn/appbarsamples/MainActivity.java
+++ AppBarSamples/app/src/main/java/com/gregttn/appbarsamples/MainActivity.java
@@ -1,15 +1,18 @@
 package com.gregttn.appbarsamples;
 
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView previewTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +21,45 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        previewTextView = (TextView) findViewById(R.id.text_preview);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                previewTextView.setText("");
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                previewTextView.setText("Searching for: " + newText);
+                return false;
+            }
+        });
+
+        MenuItemCompat.setOnActionExpandListener(menuItem, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                Toast.makeText(MainActivity.this, "Search view expanded", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                Toast.makeText(MainActivity.this, "Search view collapsed", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
         return true;
     }
 
