ddfc9e57fd4a43b03ee9e6cfa43d33b04610a271
greg <gregttn@gmail.com>
2017-06-21 09:12:06 +0100

    Create custom class for circle view.

---
+++ CustomCircleViewSample/app/src/main/java/com/gregttn/customcircleviewsample/CircleView.java
@@ -0,0 +1,12 @@
+package com.gregttn.customcircleviewsample;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CircleView extends View {
+    public CircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
