66604e6507ea17db59a4fd7d8a0b29fd9ffe2f56
greg <gregttn@gmail.com>
2017-06-21 09:21:19 +0100

    Extract showInnerCircle atribute from the layout.

--- CustomCircleViewSample/app/src/main/java/com/gregttn/customcircleviewsample/CircleView.java
+++ CustomCircleViewSample/app/src/main/java/com/gregttn/customcircleviewsample/CircleView.java
@@ -1,12 +1,23 @@
 package com.gregttn.customcircleviewsample;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class CircleView extends View {
+    private boolean showInnerCircle = false;
+
     public CircleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
+        TypedArray attributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CircleView, 0, 0);
+
+        try {
+            showInnerCircle = attributes.getBoolean(R.styleable.CircleView_showInnerCircle, false);
+        } finally {
+            attributes.recycle();
+        }
     }
 }
