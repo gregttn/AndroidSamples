c91f3ccee302b94ee045b72a17821e5b34ea1c87
greg <gregttn@gmail.com>
2017-06-26 16:30:48 +0100

    Added snackbar trigger by pressing a button

--- SnackbarSample/app/src/main/java/com/gregttn/snackbarsample/MainActivity.java
+++ SnackbarSample/app/src/main/java/com/gregttn/snackbarsample/MainActivity.java
@@ -1,7 +1,9 @@
 package com.gregttn.snackbarsample;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onShowSnackbar(View view) {
+        View rootLayout = findViewById(R.id.root_layout);
+        Snackbar snackbar = Snackbar.make(rootLayout, R.string.test_snackbar, Snackbar.LENGTH_SHORT);
+        snackbar.show();
     }
 }
